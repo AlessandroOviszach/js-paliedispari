a61f5dd47a6d5f67e27dc2d340f6c0b178263d23
Alessandro Oviszach <78302740+AlessandroOviszach@users.noreply.github.com>
2021-11-02 18:36:42 +0100

    parte esercizio (palindroma)

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
-console.log('JS OK')
+// Palindroma
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma
+
+// dichirazione delle variabili
+const user_word = prompt('inserisci una parola');
+const reverse_word = reverse_func(user_word);
+
+if (reverse_word === user_word) {
+    console.log('Parola palindroma');
+} else {
+    console.log('Parola non palindroma');
+}
+
+// funzione di 'reverse' sulla parola passata
+function reverse_func(x) {
+    const reverse = x.split('').reverse('').join('');
+
+    return reverse;
+}
